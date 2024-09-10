5f5b67d4c489705f3d85e94535cdfb40a0c738b9
khanyisani-ui <khanyilekhanyisani8@gmail.com>
2024-09-10 11:46:35 -0700

    Add basic calculation script

---
+++ python_introduction/basic_operations.py
@@ -0,0 +1,10 @@
+# Assign values to the variables
+number1 = 10
+number2 = 5
+
+
+# Perform arithmetic operations
+
+sum = number1 + number2
+difference = number1 - number2
+product = number1 * number2
