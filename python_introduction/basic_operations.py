0c5c36bbe621815d839b2be0dd650e322bf98b14
khanyisani-ui <khanyilekhanyisani8@gmail.com>
2024-09-10 11:58:17 -0700

    Updated script in the file

--- python_introduction/basic_operations.py
+++ python_introduction/basic_operations.py
@@ -8,3 +8,8 @@ number2 = 5
 sum = number1 + number2
 difference = number1 - number2
 product = number1 * number2
+
+# Print the results
+print(f"Sum of {number1} and {number2} is {sum}")
+print(f"Difference of {number1} and {number2} is {difference}")
+print(f"Product of {number1} and {number2} is {product}")
